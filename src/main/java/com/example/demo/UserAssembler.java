56deaf6d3a651f69544afbbc19024cefcdf4eb97
seculoper <mir8725@naver.com>
2021-03-30 23:19:59 +0900

    RepresentationModelAssembler 추가

---
+++ src/main/java/com/example/demo/UserAssembler.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+/* RepresentationModelAssembler란?
+ * RepresentationModel(엔티티모델, 컬렉션모델)을 사용할 때 생기는 중복 코드를 방지하기 위한 인터페이스이다.
+ * 해당 인터페이스를 구현한 클래스를 Bean 등록하면, toModel()이란 메소드로 중복 코드를 깔끔하게 작성할 수 있다.
+ */
+@Component
+public class UserAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+    @Override
+    public EntityModel<User> toModel(User user) {
+        EntityModel<User> newuser = EntityModel.of(user
+                , linkTo(methodOn(UserController.class).findUser(user.getId())).withSelfRel()
+                , linkTo(UserController.class).withRel("create-user")
+                , linkTo(UserController.class).slash(user.getId()).withRel("update-user")
+                , linkTo(methodOn(UserController.class).deleteUser(user.getId())).withRel("delete-user")
+                , linkTo(UserController.class).withRel("users"));
+        return newuser;
+    }
+}
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,54 +23,49 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
  * consumes는 요청 헤더의 Conten-Type이 해당 미디어 타입과 일치할 때, 즉 올바른 타입일 경우만 매핑하고,
  * produces는 요청 헤더의 Accept에 해당 미디어 타입이 존재할 때, 즉 응답을 받을 수 있을 때 매핑이 된다
  */
-@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaTypes.HAL_JSON_VALUE)
+@RequestMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final UserRepository userRepository;
+    private final UserAssembler userAssembler;
 
-    public UserController(UserService userService, UserRepository userRepository) {
-        this.userService = userService;
-        this.userRepository = userRepository;
-    }
 
     @GetMapping("/")
     public String home() {
         return "<h1>Hello World!</h1>";
     }
 
-    @GetMapping("/finduser/{id}")
-    public ResponseEntity<User> findUser(@PathVariable Integer id) {
+    @GetMapping("{id}")
+    public ResponseEntity<EntityModel<User>> findUser(@PathVariable Integer id) {
         User user = userService.findUserById(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(userAssembler.toModel(user));
     }
 
     @GetMapping("/getuser/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Integer id) {
+    public ResponseEntity<EntityModel<User>> getUser(@PathVariable Integer id) {
         User user = userService.getUserById(id);
-        System.out.println("사용자 ID = " + user.getId() + "\n사용자 이름 = " + user.getName() + "\n사용자 설명 = " + user.getDescription());
-        return ResponseEntity.ok(user);
+
+        return ResponseEntity.ok(userAssembler.toModel(user));
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public CollectionModel<EntityModel<User>> findUserAll() {
         // Entity 각각의 출력할 데이터 설정
         List<EntityModel<User>> userList = userService.findUserAll().stream()
-                .map(user -> EntityModel.of(user
-                        , linkTo(methodOn(UserController.class).findUser(user.getId())).withSelfRel()
-                        , linkTo(methodOn(UserController.class).findUserAll()).withRel("users")))
+                .map(userAssembler::toModel)
                 .collect(Collectors.toList());
 
         // 전체 list의 출력할 데이터 설정
         return CollectionModel.of(userList,
-                linkTo(methodOn(UserController.class).findUserAll()).withSelfRel());
+                linkTo(UserController.class).withSelfRel());
     }
 
-    @PostMapping("/user")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    @PostMapping
+    public ResponseEntity<EntityModel<User>> createUser(@RequestBody User user) {
         User newuser = userService.createUser(user);
         URI createUri = linkTo(methodOn(UserController.class).createUser(newuser)).slash(user.getId()).toUri();
 
-        return ResponseEntity.created(createUri).body(newuser);
+        return ResponseEntity.created(createUri).body(userAssembler.toModel(newuser));
     }
 
     /* PUT과 DELETE?
@@ -77,14 +74,12 @@ public class UserController {
      * 우선 해당 레포지토리는 공부 목적이므로 RESTful을 충실히 이행하도록 한다.
      * (하지만 DELETE 메소드는 정말 좋지 않으니, 절대 사용하지 않도록 한다!)
      */
-    @PutMapping("/user/{id}")
+    @PutMapping("/{id}")
     public EntityModel<User> updateUser(@RequestBody User user, @PathVariable Integer id) {
         user.setId(id);
-        User result = userService.updateUser(user);
+        User updateUser = userService.updateUser(user);
 
-        return EntityModel.of(result,
-                linkTo(methodOn(UserController.class).getUser(user.getId())).withSelfRel(),
-                linkTo(methodOn(UserController.class).findUserAll()).withRel("users"));
+        return userAssembler.toModel(updateUser);
     }
 
     /* DELETE 메소드는 기본 차단되므로, POST 메소드로 변경한다.
@@ -92,10 +87,10 @@ public class UserController {
      * 변경 이후의 Location도 제공하므로 더욱 적합하다.
      * GET 메소드는 requestbody가 없다는 점에서 DELETE 메소드와 가장 흡사하나, 의미가 너무 동떨어지므로 적합하지 않다고 생각한다
      */
-    @PostMapping("user/{id}/delete")
+    @PostMapping("{id}/delete")
     public ResponseEntity<Link> deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
 
-        return ResponseEntity.ok().body(linkTo(methodOn(UserController.class).findUserAll()).withRel("users"));
+        return ResponseEntity.ok().body(linkTo(UserController.class).withRel("users"));
     }
 }
